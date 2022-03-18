4dd4a0806e8ce730c8fe20b261f30cd3184d0156
rexsheikh <56654312+rexsheikh@users.noreply.github.com>
2022-03-18 11:19:13 -0400

    Added a view function to get all super heroes/villains to return a custom dictionary.

--- supers/views.py
+++ supers/views.py
@@ -1,16 +1,37 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Super
+from super_types.models import Super_Type
 from .serializers import SuperSerializer
 from rest_framework import status
 from django.shortcuts import get_object_or_404
 
 @api_view(['GET','POST'])
 def super_list(request):
+    custom_response_dict = {}
+    supers = Super.objects.all()
+    super_types = Super_Type.objects.all()
+
     if request.method == 'GET':
-        supers = Super.objects.all()
+        super_param = request.query_params.get('type')
+        if super_param:
+            if super_param == 'hero':
+                supers = supers.filter(super_type__type = 'Hero')
+            elif super_param == 'villain':
+                supers = supers.filter(super_type__type = 'Villain')
+        else:
+            for super_type in super_types:
+                supers = Super.objects.all()
+                supers = supers.filter(super_type__type = super_type.type)
+                serializer = SuperSerializer(supers,many = True)
+                custom_response_dict[super_type.type] = serializer.data
+            return Response(custom_response_dict)
         serializer = SuperSerializer(supers,many=True)
         return Response(serializer.data)
+
+        
+        #anything with hero type gets added to hero key in customdict
+        #anything with villain type gets added to villain key in customdict 
 
     elif request.method == 'POST':
         serializer = SuperSerializer(data=request.data)
